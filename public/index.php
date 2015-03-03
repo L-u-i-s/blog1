48b1948a8bd224100f23db49c7dc696b9a974cf1
Luis <luisdaniel.240395@gmail.com>
2015-03-03 18:18:37 +0000

    configures blade templating for slim framework, checks if template variables passing is working

--- public/index.php
+++ public/index.php
@@ -3,12 +3,30 @@
     require("../vendor/autoload.php");
     
     # instantiates a new Slim Application
-    $app = new \Slim\Slim();
+    $app = new \Slim\Slim(
+        array(
+            #Adds application settings
+            'view' => new \Slim\Views\Blade(),
+            'templates.path' => '../templates',
+        )
+    );    
+    
+    #setup templates compiled cache
+    $view = $app->view();
+    $view->parserOptions = array(
+        'debug' => true,
+        'cache' => '../html_cache'
+    );    
     
     # defines a route for the GET method
-    $app->get("/", function(){
-        $view = require("index.html");
-        echo $view;
+    $app->get("/", function() use ($app){ 
+        $app->render(
+            'index',
+            array(
+                'title' => 'This is title variable',
+                'HeroUnit' => 'Welcome to my Todo App!!'
+            )    
+        );    
     });
     
     #Actually runs the Application
